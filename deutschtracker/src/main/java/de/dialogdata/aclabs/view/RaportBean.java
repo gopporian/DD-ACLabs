bec331e764eea964c635c139212007a06ed3bb23
dialogdata <roxana.lorint@student.upt.ro>
2014-05-14 18:11:39 +0300

    template reports

---
+++ deutschtracker/src/main/java/de/dialogdata/aclabs/view/RaportBean.java
@@ -0,0 +1,20 @@
+package de.dialogdata.aclabs.view;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+import de.dialogdata.aclabs.entities.UserBE;
+
+
+@ManagedBean(name="raportBean")
+public class RaportBean {
+		
+	
+	public List<UserBE> generate()
+	{
+		return null;
+		
+	}
+
+}
